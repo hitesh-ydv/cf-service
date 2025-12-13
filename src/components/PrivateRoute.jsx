1f7713842a35c8d4b4bad9fa17964e9e4ca10cee
hitesh-ydv <hiteshyadav124001@gmail.com>
2025-12-13 13:40:38 +0530

    first commit

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,6 +1,22 @@
 import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function PrivateRoute({ children }) {
-    const token = localStorage.getItem("adminToken");
-    return token ? children : <Navigate to="/login" />;
+  const [authorized, setAuthorized] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://call-forward.onrender.com/api/me", {
+        withCredentials: true
+      })
+      .then(() => setAuthorized(true))
+      .catch(() => setAuthorized(false))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return null; // or spinner
+
+  return authorized ? children : <Navigate to="/login" replace />;
 }
